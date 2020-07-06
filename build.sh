0abf5892ed0399fb0bca66037356356b453bc541
Jack McDade <jack@jackmcdade.com>
2020-07-06 10:23:16 -0400

    update build script

--- build.sh
+++ build.sh
@@ -1,4 +1,28 @@
+#!/bin/sh
+
+# Install PHP & WGET
 yum install php wget
-wget https://raw.githubusercontent.com/composer/getcomposer.org/76a7060ccb93902cd7576b67264ad91c8a2700e2/web/installer -O - -q | php -- --quiet
+
+# INSTALL COMPOSER
+EXPECTED_CHECKSUM="$(wget -q -O - https://composer.github.io/installer.sig)"
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+
+if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
+then
+    >&2 echo 'ERROR: Invalid installer checksum'
+    rm composer-setup.php
+    exit 1
+fi
+
+php composer-setup.php --quiet
+rm composer-setup.php
+
+# INSTALL COMPOSER DEPENDENCIES
 php composer install
+
+# BUILD FRONTEND
+npm install && npm run prod
+
+# BUILD STATIC SITE
 php please ssg:generate
