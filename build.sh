b2b4585eea8d923fa5988d65423c80054fc0ff59
Jack McDade <jack@jackmcdade.com>
2020-07-06 10:25:34 -0400

    update build script

--- build.sh
+++ build.sh
@@ -3,26 +3,28 @@
 # Install PHP & WGET
 yum install php wget
 
+php -v
+
 # INSTALL COMPOSER
-EXPECTED_CHECKSUM="$(wget -q -O - https://composer.github.io/installer.sig)"
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+# EXPECTED_CHECKSUM="$(wget -q -O - https://composer.github.io/installer.sig)"
+# php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+# ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
 
-if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
-then
-    >&2 echo 'ERROR: Invalid installer checksum'
-    rm composer-setup.php
-    exit 1
-fi
+# if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
+# then
+#     >&2 echo 'ERROR: Invalid installer checksum'
+#     rm composer-setup.php
+#     exit 1
+# fi
 
-php composer-setup.php --quiet
-rm composer-setup.php
+# php composer-setup.php --quiet
+# rm composer-setup.php
 
-# INSTALL COMPOSER DEPENDENCIES
-php composer install
+# # INSTALL COMPOSER DEPENDENCIES
+# php composer.phar install
 
-# BUILD FRONTEND
-npm install && npm run prod
+# # BUILD FRONTEND
+# npm install && npm run prod
 
-# BUILD STATIC SITE
-php please ssg:generate
+# # BUILD STATIC SITE
+# php please ssg:generate
