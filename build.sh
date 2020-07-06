c6bab7b3fab3927eca5a158d6c801b9ef0d537fc
Jack McDade <jack@jackmcdade.com>
2020-07-06 10:43:19 -0400

    update build script

--- build.sh
+++ build.sh
@@ -11,25 +11,25 @@ yum install php php-{common,curl,mbstring,gd,gettext,bcmath,json,xml,fpm,intl,zi
 php --version
 
 # INSTALL COMPOSER
-# EXPECTED_CHECKSUM="$(wget -q -O - https://composer.github.io/installer.sig)"
-# php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-# ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+EXPECTED_CHECKSUM="$(wget -q -O - https://composer.github.io/installer.sig)"
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
 
-# if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
-# then
-#     >&2 echo 'ERROR: Invalid installer checksum'
-#     rm composer-setup.php
-#     exit 1
-# fi
+if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
+then
+    >&2 echo 'ERROR: Invalid installer checksum'
+    rm composer-setup.php
+    exit 1
+fi
 
-# php composer-setup.php --quiet
-# rm composer-setup.php
+php composer-setup.php --quiet
+rm composer-setup.php
 
-# # INSTALL COMPOSER DEPENDENCIES
-# php composer.phar install
+# INSTALL COMPOSER DEPENDENCIES
+php composer.phar install
 
-# # BUILD FRONTEND
+# BUILD FRONTEND
 # npm install && npm run prod
 
-# # BUILD STATIC SITE
-# php please ssg:generate
+# BUILD STATIC SITE
+php please ssg:generate
