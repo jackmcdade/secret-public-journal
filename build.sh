5e4a472b5f8382faa7dde53635ae9b458a61e245
Jack McDade <jack@jackmcdade.com>
2020-07-06 10:20:10 -0400

    update build script

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 yum install php wget
 wget https://raw.githubusercontent.com/composer/getcomposer.org/76a7060ccb93902cd7576b67264ad91c8a2700e2/web/installer -O - -q | php -- --quiet
-composer install
+php composer install
 php please ssg:generate
